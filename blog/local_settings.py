e0197a9b91045fafbbb72a6bd4e7d74bde84e453
GustavoGentofanti <gustavogentofanti.pro@gmail.com>
2022-08-05 19:11:27 -0300

    Blog

---
+++ blog/local_settings.py
@@ -0,0 +1,21 @@
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'gustavo2503',
+    }
+}
+
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = None
+SESSION_COOKIE_SECURE = None
+CSRF_COOKIE_SECURE = None
+
+
